3875b18b205f4333ca5172022c35f831c7fd5a0e
pelshad <akuaru@gmail.com>
2023-01-12 22:06:22 +0900

    동적 페이지 기초

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,4 +12,15 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";// resources/templates/{hello}.html에 렌더링
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        //@RequestParam() -> GET방식으로 받는 parameter(쿼리스트링)
+        //@RequestParam(key값) String value값
+        //addAtribute(view 단의 변수명 ${}, 출력될 값)
+        model.addAttribute("name", name);
+        return "hello-template";
+        //랜더링 페이지로 가는 주소 : hello-mvc?name={name}
+    }
+
 }
